4072ed2e6f42582497850164e340b5233a631bd8
minhdung25 <93820313+minhdung25@users.noreply.github.com>
2023-02-28 22:00:18 +0700

    Update script.js

--- script.js
+++ script.js
@@ -42,7 +42,7 @@ $(document).ready(function(){
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["a Freshman","learn at Software Engineering","learn at FPTU"],
+        strings: ["a Sophomore","learn at Software Engineering","learn at FPTU"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
